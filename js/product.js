651b1aa9d3cff1c271058313f5bbd5a1e9dc58aa
kyrylolvov <kyrylo@arccompute.io>
2022-06-16 11:00:08 -0400

    fixed links

--- js/product.js
+++ js/product.js
@@ -1,27 +1,25 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
 
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
-
-
-function openModal(modal){
-    if (modal == null) return
-    modal.classList.add('active')
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
 }
 
-function closeModal(modal){
-  if (modal == null) return
-  modal.classList.remove('active')
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
 }
